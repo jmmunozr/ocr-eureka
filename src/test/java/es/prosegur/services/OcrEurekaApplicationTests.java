4f8167f9d4b9bbe536056f3300e0abffc126d409
jmmunozr <jose-maria.munoz-ruiz@ext.prosegur.com>
2016-12-22 11:23:27 +0100

    Initial commit

---
+++ src/test/java/es/prosegur/services/OcrEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package es.prosegur.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OcrEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
